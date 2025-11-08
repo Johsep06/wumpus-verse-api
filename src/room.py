1db17849b078085e154a9343b827f13dc029724c
jose maciel <jose.r.maciel@outlook.com>
2025-11-08 17:44:41 -0300

    :fix: correção no objeto Room

--- src/room.py
+++ src/room.py
@@ -1,4 +1,3 @@
 class Room:
-    def __init__(self, entidade:list, percepcao:list, *args, **kwargs):
+    def __init__(self, entidade:list, *args, **kwargs):
         self.entidade = '' if entidade is None else entidade
-        self.percepcao = '' if percepcao is None else percepcao
