66c66d1381a527d04cc06282a2d728b52b2c979b
jose maciel <jose.r.maciel@outlook.com>
2025-12-02 20:48:29 -0300

    :refactor: adicionado __all__ para limitar as importações dos schemas

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -3,3 +3,15 @@ from .room import RoomSchemas
 from .token import TokenSchemas
 from .user_create import UserCreateSchemas
 from .user_response import UserResponseSchemas
+from .user_login import UserLoginSchemas
+from .firebase_user import FirebaseUserSchemas
+
+__all__ = [
+    'EnviromentSchemas',
+    'RoomSchemas',
+    'TokenSchemas',
+    'UserCreateSchemas',
+    'UserResponseSchemas',
+    'UserLoginSchemas',
+    'FirebaseUserSchemas',
+]
