7c77e2145aac549387d23bb02960d272a40bdc41
jose maciel <jose.r.maciel@outlook.com>
2025-12-02 20:52:19 -0300

    :refactor: movido importações de variáveis de ambiente para o arquivo main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+# Carregando variáveis de ambiente
+load_dotenv()
+
+FIREBASE_API_KEY = os.getenv('FIREBASE_API_KEY')
+
+DB_CONFIG = {
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'port': int(os.getenv('DB_PORT')),
+    'name':os.getenv('DB_NAME')
+}
 
 # inicialização do FastAPI
 app = FastAPI()
