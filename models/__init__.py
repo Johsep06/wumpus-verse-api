81ec1355829cecbca6e1711390afd4ebccb847c8
jose maciel <jose.r.maciel@outlook.com>
2025-12-02 20:53:49 -0300

    :refactor: modificações para importação das credenciais do banco de dados

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,7 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
-load_dotenv()
-
-DB_CONFIG = {
-    'host': os.getenv('DB_HOST'),
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'port': int(os.getenv('DB_PORT')),
-    'name':os.getenv('DB_NAME')
-}
+from main import DB_CONFIG
 
 db = create_engine(f'postgresql://{DB_CONFIG["user"]}:{DB_CONFIG["password"]}@{DB_CONFIG["host"]}:{DB_CONFIG["port"]}/{DB_CONFIG["name"]}')
 
