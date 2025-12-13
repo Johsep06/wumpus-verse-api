919035405d21476d7b9b7c26f731224e5ca5323a
jose maciel <jose.r.maciel@outlook.com>
2025-12-13 00:24:21 -0300

    :style: fomatação dos arquivos de conecção ao firebase levemente alterados

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -1,8 +1,9 @@
-from .config import initialize_firebase, firebase_auth, firestore_db, db
+from .config import initialize_firebase, firebase_auth, firestore_db, db, exceptions
 
 __all__ = [
     'initialize_firebase',
     'firebase_auth', 
     'firestore_db',
-    'db'
+    'db',
+    'exceptions',
 ]
--- firebase/config.py
+++ firebase/config.py
@@ -1,21 +1,22 @@
 import firebase_admin
-from firebase_admin import credentials, auth, firestore
-import os
+from firebase_admin import credentials, auth, firestore, exceptions
+
 
 def initialize_firebase():
     try:
         # Caminho para o arquivo de credenciais
         cred_path = "firebaseKey.json"
-        
+
         # Verifica se o Firebase já foi inicializado
         if not firebase_admin._apps:
             cred = credentials.Certificate(cred_path)
             firebase_admin.initialize_app(cred)
-        
+
         return auth, firestore
     except Exception as e:
         print(f"Erro ao inicializar Firebase: {e}")
         raise
+
 
 # Inicializa o Firebase
 firebase_auth, firestore_db = initialize_firebase()
