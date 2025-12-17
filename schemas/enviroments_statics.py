354b9633476c30b7ebbb9655711b34149e7c8812
jose maciel <jose.r.maciel@outlook.com>
2025-12-17 17:06:33 -0300

    :fix: novo schema do ambiente adicionado

---
+++ schemas/enviroments_statics.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+
+from .entity_density import EntityDensitySchemas
+from .number_entities import NumberEntitiesSchemas
+from .room import RoomSchemas
+
+
+class EnviromentsStatics(BaseModel):
+    totalSalas: int
+    salasAtivas: int
+    salasInativas: int
+    quantidadeEntidades: NumberEntitiesSchemas
+    densidadeEntidades: EntityDensitySchemas
+    salas: List[RoomSchemas]
+    
