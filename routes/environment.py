ed3cf3ee35fa91c039a48beced12a4948895e6d1
jose maciel <jose.r.maciel@outlook.com>
2025-12-19 12:59:47 -0300

    :feat: adicionado rota para salvar ambientes

--- routes/environment.py
+++ routes/environment.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from datetime import datetime
+from sqlalchemy.orm import Session
 
-from schemas import EnviromentSchemas, RoomSchemas
-from src import Environment, Room
+from schemas import EnviromentSchemas, UserSchemas
+from dependencies import get_session, check_token
+from models import Environment, Room, RoomObject
 
 environment_router = APIRouter(prefix='/environment', tags=['environment'])
 
@@ -16,7 +19,11 @@ async def home():
 
 
 @environment_router.post('/')
-async def new_environment(environment_schemas:EnviromentSchemas):
+async def new_environment(
+    environment_schemas: EnviromentSchemas, 
+    session: Session = Depends(get_session),
+    user: UserSchemas = Depends(check_token)
+):
     """
     Cria um novo ambiente no sistema
     Args:
@@ -28,12 +35,58 @@ async def new_environment(environment_schemas:EnviromentSchemas):
     Returns:
         dict: mensagem de sucesso.
     """
-    enviroment = Environment()
-    enviroment.altura = environment_schemas.altura
-    enviroment.largura = environment_schemas.largura
-    
-    for sala in environment_schemas.salas:
-        room = Room(sala.entidade)
-        enviroment.salas[(sala.x, sala.y)] = room
 
-    return {'msg':'ambiente criado com sucesso'}
+    objects_database_ids = {
+        'W': 1,
+        'P': 2,
+        'O': 3,
+    }
+
+    environment = Environment(
+        nome=environment_schemas.nome,
+        largura=environment_schemas.largura,
+        altura=environment_schemas.altura,
+        data_criacao=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        usuario_id=user.id
+    )
+    session.add(environment)
+    session.flush()
+
+    environment_id = environment.id
+
+    for sala in environment_schemas.salas:
+        room = Room(
+            ambiente_id=environment_id,
+            posicao_x=sala.x,
+            posicao_y=sala.y,
+        )
+        session.add(room)
+
+        if sala.buraco:
+            room_object = RoomObject(
+                ambiente_id=environment_id,
+                posicao_x=sala.x,
+                posicao_y=sala.y,
+                objeto_id=objects_database_ids['P']
+            )
+            session.add(room_object)
+        if sala.ouro:
+            room_object = RoomObject(
+                ambiente_id=environment_id,
+                posicao_x=sala.x,
+                posicao_y=sala.y,
+                objeto_id=objects_database_ids['O']
+            )
+            session.add(room_object)
+        if sala.wumpus:
+            room_object = RoomObject(
+                ambiente_id=environment_id,
+                posicao_x=sala.x,
+                posicao_y=sala.y,
+                objeto_id=objects_database_ids['W']
+            )
+            session.add(room_object)
+
+    session.commit()
+
+    return {'msg': 'ambiente criado com sucesso'}
