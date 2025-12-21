dff47ff75b6684dc2eb2952f571da0052ca9b0c3
jose maciel <jose.r.maciel@outlook.com>
2025-12-21 13:57:31 -0300

    :feat: adicionada rota para resgate de um ambiente pelo id

--- routes/environment.py
+++ routes/environment.py
@@ -3,11 +3,17 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy import func, select
 
-from schemas import EnvironmentSchemas, UserSchemas, EnviromentsStaticsSchemas, NumberEntitiesSchemas, EntityDensitySchemas, EnvironmentResponseSchemas
+from schemas import EnvironmentSchemas, UserSchemas, EnviromentsStaticsSchemas, NumberEntitiesSchemas, EntityDensitySchemas, EnvironmentResponseSchemas, RoomSchemas
 from dependencies import get_session, check_token
 from models import Environment, Room, RoomObject, User
 
 environment_router = APIRouter(prefix='/environment', tags=['environment'])
+
+OBJECTS_DATABASE_IDS = {
+    'W': 1,
+    'P': 2,
+    'O': 3,
+}
 
 
 def get_number_entities(session: Session, environment_id: int) -> NumberEntitiesSchemas:
@@ -74,7 +80,7 @@ def get_environments_statics(
     )
 
 
-def get_environment(session: Session, environment_id: int) -> EnvironmentResponseSchemas:
+def get_environment_summary(session: Session, environment_id: int) -> EnvironmentResponseSchemas:
     environment = session.query(Environment).filter(Environment.id == environment_id).first()
     
     return EnvironmentResponseSchemas(
@@ -90,6 +96,37 @@ def get_environment(session: Session, environment_id: int) -> EnvironmentRespons
         ),
     )
 
+
+def get_entities_in_environment(session: Session, environment_id:int, entity_symbol:str) -> list[tuple[int, int]]:
+    coordinates = (
+        session.query(RoomObject.posicao_x, RoomObject.posicao_y)
+            .filter(RoomObject.ambiente_id == environment_id)
+            .filter(RoomObject.objeto_id == OBJECTS_DATABASE_IDS.get(entity_symbol))
+            .all()
+    )
+    
+    return coordinates
+
+
+def get_rooms(session: Session, environment_id:int) -> list[RoomSchemas]:
+    rooms = session.query(Room).filter(Room.ambiente_id == environment_id).all()
+    wumpus_coordinates = get_entities_in_environment(session, environment_id, 'W')
+    hole_coordinates = get_entities_in_environment(session, environment_id, 'P')
+    gold_coordinates = get_entities_in_environment(session, environment_id, 'O')
+    
+    room_list = []
+    for room in rooms:
+        room_list.append(
+            RoomSchemas(
+                x=room.posicao_x,
+                y=room.posicao_y,
+                wumpus=(room.posicao_x, room.posicao_y) in wumpus_coordinates,
+                buraco=(room.posicao_x, room.posicao_y) in hole_coordinates,
+                ouro=(room.posicao_x, room.posicao_y) in gold_coordinates,
+            )
+        )
+    
+    return room_list
 
 @environment_router.get('/')
 async def home():
@@ -118,12 +155,6 @@ async def new_environment(
         dict: mensagem de sucesso.
     """
 
-    objects_database_ids = {
-        'W': 1,
-        'P': 2,
-        'O': 3,
-    }
-
     environment = Environment(
         nome=environment_schemas.nome,
         largura=environment_schemas.largura,
@@ -149,7 +180,7 @@ async def new_environment(
                 ambiente_id=environment_id,
                 posicao_x=sala.x,
                 posicao_y=sala.y,
-                objeto_id=objects_database_ids['P']
+                objeto_id=OBJECTS_DATABASE_IDS['P']
             )
             session.add(room_object)
         if sala.ouro:
@@ -157,7 +188,7 @@ async def new_environment(
                 ambiente_id=environment_id,
                 posicao_x=sala.x,
                 posicao_y=sala.y,
-                objeto_id=objects_database_ids['O']
+                objeto_id=OBJECTS_DATABASE_IDS['O']
             )
             session.add(room_object)
         if sala.wumpus:
@@ -165,7 +196,7 @@ async def new_environment(
                 ambiente_id=environment_id,
                 posicao_x=sala.x,
                 posicao_y=sala.y,
-                objeto_id=objects_database_ids['W']
+                objeto_id=OBJECTS_DATABASE_IDS['W']
             )
             session.add(room_object)
 
@@ -186,6 +217,26 @@ async def user_environments(
     environments = []
     
     for id_ in ids_enviroments:
-        environments.append(get_environment(session, id_))
+        environments.append(get_environment_summary(session, id_))
     
     return environments
+
+@environment_router.get('/environment')
+async def environment_by_id(environment_id:int, session:Session=Depends(get_session)):
+    environment = session.query(Environment).filter(Environment.id == environment_id).first()
+    environment_statics = get_environments_statics(
+        session, 
+        environment_id,
+        environment.altura,
+        environment.largura,
+    )
+    environment_rooms = get_rooms(session, environment_id)
+    
+    return EnvironmentSchemas(
+        id=environment_id,
+        nome=environment.nome,
+        largura=environment.largura,
+        altura=environment.altura,
+        estatisticas=environment_statics,
+        salas=environment_rooms,
+    )
