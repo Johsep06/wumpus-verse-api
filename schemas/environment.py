b750976d914b46777313d74e7440e475887345be
jose maciel <jose.r.maciel@outlook.com>
2025-12-26 21:23:12 -0300

    :fix: corrigido erro no preenchimento opcional de dados do esquemas do ambiente

--- schemas/environment.py
+++ schemas/environment.py
@@ -7,14 +7,17 @@ from .enviroments_statics import EnviromentsStaticsSchemas
 
 
 class EnvironmentSchemas(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     nome: str
     largura: int
     altura: int
-    data_criacao: Optional[datetime]
+    data_criacao: Optional[datetime] = None
     estatisticas: EnviromentsStaticsSchemas
     salas: List[RoomSchemas]
 
     @field_serializer('data_criacao')
-    def serialize_criado_em(self, dt: datetime, _info):
-        return dt.strftime("%Y-%m-%d %H:%M:%S")
+    def serialize_criado_em(self, dt: Optional[datetime], _info):
+        # Verifica se dt não é None antes de formatar
+        if dt is not None:
+            return dt.strftime("%Y-%m-%d %H:%M:%S")
+        return None  # ou return dt (que seria None)
