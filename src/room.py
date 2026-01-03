8056ade99aa48c6f6ab03a2ef7d910a5feb78c19
jose maciel <jose.r.maciel@outlook.com>
2026-01-03 14:32:57 -0300

    :feat: classe Room aprimorada para adicionar, ocultar e revelar percepcões e entidades

--- src/room.py
+++ src/room.py
@@ -1,4 +1,28 @@
 class Room:
-    def __init__(self, entidade:list, *args, **kwargs):
-        self.entidade = '' if entidade is None else entidade
-        self.percepcao = []
+    def __init__(self, entity: list, *args, **kwargs):
+        self.entities: list[str] = [] if entity is None else entity
+        self.perceptions: list[str] = []
+
+    def add_perception(self, perception: str):
+        if perception not in self.perceptions:
+            perception.append(perception)
+
+    def hide_perception(self, perception: str):
+        if perception in self.perceptions:
+            index = self.perceptions.index(perception)
+            self.perceptions[index] = perception.upper()
+
+    def reveal_perception(self, perception: str):
+        if perception in self.perceptions:
+            index = self.perceptions.index(perception)
+            self.perceptions[index] = perception.lower()
+
+    def hide_entity(self, entity:str):
+        if entity in self.entities:
+            index = self.entities.index(entity)
+            self.entities[index] = entity.lower()
+    
+    def reveal_entity(self, entity:str):
+        if entity in self.entities:
+            index = self.entities.index(entity)
+            self.entities[index] = entity.upper()
