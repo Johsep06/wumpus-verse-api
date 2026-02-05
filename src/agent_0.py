1f6b31080db6ad69d5ee2854f70d2d8f1bd15448
jose maciel <jose.r.maciel@outlook.com>
2026-02-05 15:23:25 -0300

    :fix: corrigido erro do agente atirar até acabar as balas

--- src/agent_0.py
+++ src/agent_0.py
@@ -8,9 +8,12 @@ class Agent0(Agent):
 
     def execute(self, data_position: dict) -> str:
         choice = random.choice(data_position['directions'])
+        shot = random.randint(0,1)
         if 'br' in data_position['perception']:
             return 'x'
-        if ('f' in data_position['perception']) and (self.arrows > 0):
+        if ('f' in data_position['perception']) and \
+            (self.arrows > 0) and \
+            (shot == 0):
             self.arrows -= 1
             choice = choice.lower()
         
