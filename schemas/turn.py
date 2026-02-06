c2592f89b8fe484aa623632fa9ba829498845049
jose maciel <jose.r.maciel@outlook.com>
2026-02-06 13:58:02 -0300

    :feat: alterado o schema de turno para incluir a posição do tiro

--- schemas/turn.py
+++ schemas/turn.py
@@ -6,6 +6,7 @@ class TurnSchemas(BaseModel):
     posicao_x: int
     posicao_y: int
     acao: str
+    tiro_position: tuple[int, int] = (0, 0)
     ouros: int = 0
     flechas: int = 0
     pontos: int = 0
