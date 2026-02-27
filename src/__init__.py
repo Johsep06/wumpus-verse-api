e446c4156b80a2c034f7d530bda1c5f1e0e97b33
jose maciel <jose.r.maciel@outlook.com>
2026-02-26 23:49:56 -0300

    :feat: adicionado agente 3 (agente genético) ao sistema

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,12 @@ from .room import Room
 from .environment import Environment
 from .agent import Agent
 from .agent_0 import Agent0
+from .agent_3 import Agent3
 
 __all__ = [
     'Room',
     'Environment',
     'Agent',
     'Agent0',
+    'Agent3',
 ]
