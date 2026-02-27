f88b1f1560b5935e87f7b7b8adc6b0f79c8c57e8
jose maciel <jose.r.maciel@outlook.com>
2026-02-26 23:43:21 -0300

    :fix: modificado o encapsulamento da variável status do agente

--- src/agent.py
+++ src/agent.py
@@ -7,20 +7,21 @@ class Agent(ABC):
         self.tag = tag
         self.type = -1
         self.game_over = False
-        self.__status = ''
+        self._status = ''
         self.pts = 0
         self.position = position
         self.start_position = position
         self.gold = 0
         self.arrows= 0
+        self.action_queue:list[str] = []
         
     @property
     def status(self) -> str:
-        return self.__status
+        return self._status
 
     @status.setter
     def status(self, status: str):
-        self.__status = status
+        self._status = status
 
         for value in ['W', 'P']: 
             if value in status:
@@ -28,23 +29,23 @@ class Agent(ABC):
         
         if (self.position == self.start_position) and (self.gold > 0):
             self.game_over = True
-            self.__status = 'V'
+            self._status = 'V'
 
     def __str__(self):
         return f'Agent(tag={self.tag}, type:{self.type}, pts={self.pts}, game_over={self.game_over})'
 
     def update_pts(self):
         self.pts -= 1
-        if 'W' in self.__status:
+        if 'W' in self._status:
             self.pts -= 1000
-        elif 'P' in self.__status:
+        elif 'P' in self._status:
             self.pts -= 1000
-        elif 'x' in self.__status:
+        elif 'x' in self._status:
             self.gold += 1
             self.pts += 1000
-        elif 'T' in self.__status:
+        elif 'T' in self._status:
             self.pts += 1000
-        elif 't' in self.__status:
+        elif 't' in self._status:
             self.pts -= 10
             
     def set_position(self, x: int, y: int):
