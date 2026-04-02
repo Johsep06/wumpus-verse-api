f13b9c5db4e18a5e3b1f8fdb7901b515b1c9095b
jose maciel <jose.r.maciel@outlook.com>
2026-04-01 23:05:16 -0300

    :feat: adicionado rota de leitura da execução pelo id

--- routes/execution.py
+++ routes/execution.py
@@ -19,6 +19,7 @@ async def home(
     offset = (page - 1) * limit
     return
 
+
 @execution_router.post('/user')
 async def save_execution(
     agent_id: int,
@@ -32,7 +33,7 @@ async def save_execution(
     execution = ExecutionDB(
         id_agente=agent_id,
         id_ambiente=environment_id,
-        user_id= user_schemas.id,
+        user_id=user_schemas.id,
         posicao_x=execution_schemas[0].posicao_x,
         posicao_y=execution_schemas[0].posicao_y,
         qtd_ouros=execution_schemas[-1].ouros,
@@ -48,3 +49,38 @@ async def save_execution(
         'status_code': 200,
         'detail': 'execução salva com sucesso'
     }
+
+
+@execution_router.get('/user')
+async def get_execution_by_id(
+    execution_id: int,
+    session: Session = Depends(get_session),
+):
+    execution_data = session.query(ExecutionDB) \
+        .filter(ExecutionDB.id == execution_id) \
+        .first()
+
+    if execution_data is None:
+        return HTTPException(status_code=404, detail='Execução não encontrada')
+
+    histor: str = execution_data.historico
+    histor = histor[2:]
+    histor = histor.split(',')
+    histor = ['', *histor]
+
+    execution = ExecutionDBSchemas(
+        id=execution_data.id,
+        user_id=execution_data.user_id,
+        agente_id=execution_data.id_agente,
+        ambiente_id=execution_data.id_ambiente,
+        posicao_x=execution_data.posicao_x,
+        posicao_y=execution_data.posicao_y,
+        qtd_ouro=execution_data.qtd_ouros,
+        qtd_flechas=execution_data.qtd_flechas,
+        qtd_wumpus=execution_data.wumpus,
+        pontos=execution_data.pontos,
+        data=execution_data.data,
+        historico=histor
+    )
+
+    return execution
