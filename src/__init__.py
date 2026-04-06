188150c4588628628f954ff278552aeb348f3a35
jose maciel <jose.r.maciel@outlook.com>
2026-04-06 19:51:00 -0300

    :feat: adicionado o agente 2 ao arquivo de configuração do diretório

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from .room import Room
 from .environment import Environment
 from .agent import Agent
 from .agent_1 import Agent1
+from .agent_2 import Agent2
 from .agent_3 import Agent3
 from .pattern import validate_fitness
 
@@ -10,6 +11,7 @@ __all__ = [
     'Environment',
     'Agent',
     'Agent1',
+    'Agent2',
     'Agent3',
     'validate_fitness',
 ]
