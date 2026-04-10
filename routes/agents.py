d178dcbb55bd529af54a31caa3f031a7d6e8ca92
jose maciel <jose.r.maciel@outlook.com>
2026-04-10 00:54:37 -0300

    :feat:refactor: adicionado o segundo agente as rotas de agente e rotas refatoradas

--- routes/agents.py
+++ routes/agents.py
@@ -3,38 +3,16 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy import desc
 
-from schemas import UserSchemas, AgentDBSchemas, ThirdAgentSchemas, SecondAgentSchemas
+from schemas import UserSchemas, AgentDBSchemas, ThirdAgentSchemas, SecondAgentSchemas, AgentSchemas
 from dependencies import get_session, check_token
-from models import AgentDB, SecondAgentDB, ThirdAgentDB
+from models import AgentDB, SecondAgentDB, ThirdAgentDB, \
+    build_agent_record, build_agent_schemas, \
+    build_second_agent_record, build_second_agent_schemas, \
+    build_third_agent_record, build_third_agent_schemas
 from src import validate_fitness
 
 agents_router = APIRouter(prefix='/agents', tags=['agents'])
 
-def register_second_agent_db(user_id:int, agent_id:int, second_agent_schemas:SecondAgentSchemas):
-    second_agent_db = SecondAgentDB(
-        id = agent_id,
-        user_id = user_id,
-        memoria = True,
-        coragem = second_agent_schemas.corajoso,
-        explorador = second_agent_schemas.explorador,
-        assassino = second_agent_schemas.cacador,
-        busca_ouro = second_agent_schemas.garimpeiro,
-        forma_de_busca = 1
-    )
-    
-    return second_agent_db
-
-def register_third_agent_db(user_id:int, agent_id:int, third_agent_schemas:ThirdAgentSchemas):
-    third_agent_db = ThirdAgentDB(
-        agent_id=agent_id,
-        populacao=third_agent_schemas.populacao,
-        geracoes=third_agent_schemas.geracoes,
-        taxa_de_cruzamento=third_agent_schemas.taxa_de_cruzamento,
-        taxa_de_mutacao=third_agent_schemas.taxa_de_mutacao,
-        fitness=third_agent_schemas.fitness
-    )
-
-    return third_agent_db
 
 @agents_router.get('/')
 async def home(
@@ -49,13 +27,15 @@ async def home(
 @agents_router.post('/user')
 async def new_agent(
     agent_type: int,
-    second_agent_schemas: SecondAgentSchemas,
-    third_agent_schemas: ThirdAgentSchemas,
+    name:str, 
+    second_agent_schemas: SecondAgentSchemas = None,
+    third_agent_schemas: ThirdAgentSchemas = None,
     session: Session = Depends(get_session),
     user_schemas: UserSchemas = Depends(check_token),
 ):
-    agent = AgentDB(
+    agent = build_agent_record(
         user_id=user_schemas.id,
+        nome=name,
         data=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         tipo=agent_type,
     )
@@ -64,8 +44,7 @@ async def new_agent(
     session.flush()
 
     if agent_type == 2:
-        second_agent = register_second_agent_db(
-            user_schemas.id,
+        second_agent = build_second_agent_record(
             agent.id,
             second_agent_schemas,
         )
@@ -76,8 +55,7 @@ async def new_agent(
         fitness_is_valid = validate_fitness(third_agent_schemas.fitness.replace(' ', ''))
         if not fitness_is_valid:
             raise HTTPException(status_code=400, detail='Função fitness inválida')
-        third_agent = register_third_agent_db(
-            user_schemas.id,
+        third_agent = build_third_agent_record(
             agent.id,
             third_agent_schemas
         )
@@ -95,49 +73,61 @@ async def new_agent(
 async def get_agent(
     agent_id: int,
     session: Session = Depends(get_session),
-):
-    agent_data = session.query(AgentDB).filter(AgentDB.id == agent_id).first()
+) :
+    agent_data = session.query(AgentDB) \
+        .filter(AgentDB.id == agent_id) \
+        .first()
 
     if agent_data is None:
         return HTTPException(status_code=404, detail='Agente não encontrado')
 
-    agent = AgentDBSchemas(
-        agent_id=agent_data.id,
-        user_id=agent_data.user_id,
-        data=agent_data.data,
-        tipo=agent_data.tipo
-    )
-    if agent_data.tipo == 3:
-        properties = session.query(ThirdAgentDB).filter(
-            ThirdAgentDB.agent_id == agent_data.id).first()
-        agent.properties = ThirdAgentSchemas(
-            populacao=properties.populacao,
-            geracoes=properties.geracoes,
-            taxa_de_cruzamento=properties.taxa_de_cruzamento,
-            taxa_de_mutacao=properties.taxa_de_mutacao,
-            fitness=properties.fitness,
-        )
+    agent = build_agent_schemas(agent_data)
+    if agent_data.tipo == 2:
+        properties = session.query(SecondAgentDB) \
+            .filter(SecondAgentDB.id == agent_data.id)\
+            .first()
+        agent.properties = build_second_agent_schemas(properties)
 
-    return {
-        'agent': agent,
-    }
+    elif agent_data.tipo == 3:
+        properties = session.query(ThirdAgentDB) \
+            .filter(ThirdAgentDB.agent_id == agent_data.id)\
+            .first()
+        agent.properties = build_third_agent_schemas(properties)
+
+    return agent
 
 
 @agents_router.put('/user')
 async def update_agent(
     agent_id: int,
-    third_agent_schemas: ThirdAgentSchemas,
+    name: str,
+    second_agent_schemas: SecondAgentSchemas = None,
+    third_agent_schemas: ThirdAgentSchemas = None,
     session: Session = Depends(get_session),
     user_schemas: UserSchemas = Depends(check_token),
 ):
-    agent = session.query(AgentDB).filter(AgentDB.id == agent_id).first()
+    agent = session.query(AgentDB) \
+        .filter(AgentDB.id == agent_id) \
+        .first()
 
     if agent is None:
         raise HTTPException(status_code=404, detail='O agente não existe no banco de dados')
     if user_schemas.id != agent.user_id:
         raise HTTPException(status_code=403, detail='O usuário não tem permisão para realizar essa ação')
 
-    if agent.tipo == 3:
+    agent.nome = name
+    if agent.tipo == 2:
+        properties = session.query(SecondAgentDB) \
+            .filter(SecondAgentDB.id == agent_id) \
+            .first()
+
+        properties.corajoso = second_agent_schemas.corajoso
+        properties.explorador = second_agent_schemas.explorador
+        properties.cacador = second_agent_schemas.cacador
+        properties.garimpeiro = second_agent_schemas.garimpeiro
+        properties.forma_de_busca = second_agent_schemas.forma_de_busca
+
+    elif agent.tipo == 3:
         properties = session.query(ThirdAgentDB).filter(ThirdAgentDB.agent_id == agent_id).first()
 
         properties.populacao = third_agent_schemas.populacao
@@ -145,11 +135,11 @@ async def update_agent(
         properties.taxa_de_cruzamento = third_agent_schemas.taxa_de_cruzamento
         properties.fitness = third_agent_schemas.fitness
 
-        session.commit()
-        return {
-            'status code': 200,
-            'detail': 'Agente atualizado com sucesso'
-        }
+    session.commit()
+    return {
+        'status code': 200,
+        'detail': 'Agente atualizado com sucesso'
+    }
 
 
 @agents_router.delete('/user')
@@ -187,33 +177,32 @@ async def list_agents(
         .filter(AgentDB.user_id == user_schemas.id) \
         .order_by(desc(AgentDB.data)) \
         .all()
+    
+    if agents_list is None:
+        return []
 
-    agents: list[AgentDBSchemas] = []
+    agents: list[AgentSchemas] = []
     for i in range(offset, (offset + limit)):
         try:
             agent = agents_list[i]
         except IndexError:
             break
 
-        agent_data = AgentDBSchemas(
-            agent_id=agent.id,
-            user_id=agent.user_id,
-            data=agent.data,
-            tipo=agent.tipo
-        )
+        agent_schemas = build_agent_schemas(agent)
 
-        if agent.tipo == 3:
+        if agent_schemas.tipo == 2:
+            second_agent = session.query(SecondAgentDB) \
+                .filter(SecondAgentDB.id == agent.id) \
+                .first()
+
+            agent_schemas.properties = build_second_agent_schemas(second_agent)
+
+        elif agent_schemas.tipo == 3:
             third_agent = session.query(ThirdAgentDB) \
                 .filter(ThirdAgentDB.agent_id == agent.id) \
                 .first()
-            agent_data.properties = ThirdAgentSchemas(
-                populacao=third_agent.populacao,
-                geracoes=third_agent.populacao,
-                taxa_de_cruzamento=third_agent.taxa_de_cruzamento,
-                taxa_de_mutacao=third_agent.taxa_de_mutacao,
-                fitness=third_agent.fitness
-            )
-        agents.append(agent_data)
+            agent_schemas.properties = build_third_agent_schemas(third_agent)
+        agents.append(agent_schemas)
 
     end_of_list = (offset + limit) >= len(agents_list)
     agents.append(end_of_list)
